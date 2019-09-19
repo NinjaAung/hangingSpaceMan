6a7c8c947337c207156d2149ac16070bc2a980e5
ninjaaung <ninja.aung02@gmail.com>
2019-09-18 20:15:38 -0700

    Unitest Word Loaded

--- test_spaceman.py
+++ test_spaceman.py
@@ -1,12 +1,21 @@
 import spaceman
+import test
 import unittest
 import re
 
-''' User input checker using regex: Fail case: None letter input'''
-class RegexCheckTest(unittest.TestCase):
 
+
+
+''' User input checker using regex: Fail Case: None letter input'''
+class regex_test(unittest.TestCase):
     def test_regex(self):
         self.assertRegex('a', r'^[a-zA-Z]*$', 'Letter passed regex')
+
+
+''' Check if word was in fact loaded: Fail Case: Arg not passed resulting in a None'''
+class word_loaded_test(unittest.TestCase):
+    def test_load_word(self):
+        self.assertIsNotNone(spaceman.load_word(), 'Word was loaded')
 
 
 if __name__ == '__main__':
